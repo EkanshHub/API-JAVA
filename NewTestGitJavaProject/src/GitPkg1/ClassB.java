11cc65f38a99937ba37a2cebcaa1c085dd719eb8
eagra <eagra@LAPTOP-FG0CP0H3.hgu_lan>
2023-02-05 12:26:20 +0530

    second commit to check

---
+++ NewTestGitJavaProject/src/GitPkg1/ClassB.java
@@ -0,0 +1,11 @@
+package GitPkg1;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Second class addded to check commit is done or not");
+	}
+
+}
