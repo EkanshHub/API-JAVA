bab385e3c3ce4737bb210118e8a560ce55b77dfb
eagra <eagra@LAPTOP-FG0CP0H3.hgu_lan>
2023-02-05 12:10:15 +0530

    pushing code for the first time

---
+++ NewTestGitJavaProject/src/GitPkg1/ClassA.java
@@ -0,0 +1,12 @@
+package GitPkg1;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("I am tge main method");
+	}
+
+}
---
+++ NewTestGitJavaProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author eagra
+ *
+ */
+module NewTestGitJavaProject {
+}
